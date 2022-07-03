8d7812be3a17ecd50a87fa45abbd5ac89c2671f9
zll600 <3400692417@qq.com>
2022-07-03 14:43:51 +0800

    add vmprint function

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+_vmprint(pagetable_t pagetable, int level)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V)) {
+      printf("..");
+      for (int i = 1; i < level; ++i) {
+          printf(" ..");
+      }
+
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+          _vmprint((pagetable_t)(child), level + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable); 
+  _vmprint(pagetable, 1);
+}
---
+++ pgtbl.md
@@ -0,0 +1,5 @@
+### Print a page table
+
+- 可以使用 kernel/riscv.h 中的宏
+- 可以参考 freewalk 中的实现
+-   
